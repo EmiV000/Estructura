84361446727563444e52d447c99580a7c7922f25
EmiV000 <112599872+EmiV000@users.noreply.github.com>
2022-08-31 22:28:30 -0500

    Add files via upload

---
+++ TALLER 3/js/scripts.js
@@ -0,0 +1,15 @@
+
+ScrollReveal().reveal(".banner", {delay: 500});
+ScrollReveal().reveal(".tit_bebida", { delay: 500 })
+ScrollReveal().reveal(".nom_bebida", { delay: 500 })
+ScrollReveal().reveal(".img_bebida", { delay: 500 })
+ScrollReveal().reveal(".info_bebida", { delay: 500 })
+
+ScrollReveal().reveal(".tit_comida", { delay: 500 })
+ScrollReveal().reveal(".info_comida", { delay: 500 })
+ScrollReveal().reveal(".nom_comida", { delay: 500 })
+ScrollReveal().reveal(".img_comida", { delay: 500 })
+
+ScrollReveal().reveal(".tit_clientes", { delay: 500 })
+ScrollReveal().reveal(".info_clientes1", { delay: 500 })
+ScrollReveal().reveal(".img_clientes", { delay: 500 })
